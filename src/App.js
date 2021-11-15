ef232d1b6e5418b1d01f97961674decedc487385
DanilaPekhtserau <danila.pekhtserau@gmail.com>
2021-11-15 15:06:30 +0300

    Part of big commit.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {} from 'react';
 import './App.css';
-import Table1 from "./Components/Tabel/Table";
+import Table1 from "./Components/Table/Table";
 
 
 
